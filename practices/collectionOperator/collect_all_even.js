5b926d50a0b83fa76c937ca0a289ce6708526a24
ldliang93 <ldliang93@163.com>
2018-03-28 21:46:16 +0800

    第第一次提交

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,14 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var array = [];
+  for( i=0; i<collection.length; i++){
+    if( collection[i]%2 == 0){
+      array.push(collection[i]);
+    }
+  }
+  //console.log(array);
+  return array;
 }
 
 module.exports = collect_all_even;
--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -2,6 +2,8 @@
 
 function collect_last_element(collection) {
   //在这里写入代码
+  var len = collection.length;
+  return collection[len - 1];
 }
 
 module.exports = collect_last_element;
